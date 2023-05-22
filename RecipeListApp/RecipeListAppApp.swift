f60713196b181ff34a8b095694308f8c753ce176
BarisCiftci <barishciftci@gmail.com>
2023-05-22 20:51:52 +0200

    Initial Commit

---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by Baris Ciftci on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
